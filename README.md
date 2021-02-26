68e6ddd68244c3c460aa0003393b8a7e05c6c87b
Peter Vegh <pvegh@posteo.uk>
2021-02-26 12:35:59 +0000

    Bring code from Examples repo

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # Minotaor
-An amino acid sequence annotator
+
+**Under construction**
+
+Minotaor is an a**mino** acid sequence anno**ta**t**or** for quickly identifying common protein tags and linkers in an ORF. Additionally, it can flag peptide sequences or patterns that are known to cause problems during translation. It uses Biopython.
+
+
+## Usage
+
+```python
+import minotaor
+protein = minotaor.Seq("SYYHHHHHHDYDIPTTENLYFQG*EDINBURGHGENQMEFQUNDRY*")
+protein_record = minotaor.SeqRecord(protein, id="example", annotations={"molecule_type": "protein"})
+
+protein_record = minotaor.annotate_record(protein_record)
+
+# Define a custom translator:
+from dna_features_viewer import BiopythonTranslator
+class MinotaorTranslator(BiopythonTranslator):
+    """Custom translator.
+
+    Color warnings in red, CDS in default color, all other features in blue.
+    """
+
+    def compute_feature_color(self, feature):
+        if feature.type == "warning":
+            return "red"
+        elif feature.type == "CDS":
+            return "#7245dc"
+        else:
+            return "blue"
+
+    def compute_feature_label(self, feature):
+        return feature.id
+
+graphic_record = MinotaorTranslator().translate_record(protein_record)
+ax, _ = graphic_record.plot(figure_width=10, strand_in_label_threshold=7)
+graphic_record.plot_sequence(ax)
+```
+![Example](example.png)
---
+++ minotaor.py
@@ -0,0 +1,148 @@
+import os
+import re
+import pandas
+
+from Bio.Seq import Seq
+from Bio.SeqFeature import SeqFeature, FeatureLocation
+from Bio.SeqRecord import SeqRecord
+
+script_dir = os.path.dirname(__file__)
+seq_dataset = pandas.read_csv(os.path.join(script_dir, "seq.csv"))
+
+
+def annotate_record(seqrecord, seq_dataset=seq_dataset):
+    """Annotate a record with a reference sequence dataset.
+
+
+    **Parameters**
+
+    **seqrecord**
+    > SeqRecord to annotate.
+
+    **seq_dataset**
+    > A minotaor sequence dataset (`pandas.DataFrame`).
+    """
+    # FLAG NO START: M
+    if str(seqrecord.seq)[0] != "M":
+        seqrecord.features.append(
+            SeqFeature(FeatureLocation(0, 1), type="warning", id="no start codon")
+        )
+    # FLAG NO END: *
+    if str(seqrecord.seq)[-1] != "*":
+        seqrecord.features.append(
+            SeqFeature(
+                FeatureLocation(len(seqrecord) - 1, len(seqrecord)),
+                type="warning",
+                id="not a stop codon",
+            )
+        )
+    # FLAG STOP CODONS: *
+    stop_positions = [i for i, letter in enumerate(str(seqrecord.seq)) if letter == "*"]
+    for position in stop_positions:
+        seqrecord.features.append(
+            SeqFeature(
+                FeatureLocation(position, position + 1), type="warning", id="STOP"
+            )
+        )
+    # ANNOTATE SEQUENCES
+    sequences = seq_dataset.loc[seq_dataset["type"] == "seq"]["sequence"].to_list()
+    names = seq_dataset.loc[seq_dataset["type"] == "seq"]["name"].to_list()
+    for index, sequence in enumerate(sequences):
+        len_sequence = len(sequence)
+        name = names[index]
+        matches = [
+            m.start() for m in re.finditer(re.escape(sequence), str(seqrecord.seq))
+        ]
+        for match in matches:
+            seqrecord.features.append(
+                SeqFeature(
+                    FeatureLocation(match, (match + len_sequence)), type="CDS", id=name
+                )
+            )
+    # ANNOTATE PATTERNS
+    patterns = seq_dataset.loc[seq_dataset["type"] == "pattern"]["sequence"].to_list()
+    names = seq_dataset.loc[seq_dataset["type"] == "pattern"]["name"].to_list()
+    for index, pattern in enumerate(patterns):
+        name = names[index]
+        matches = {m.start(): m.end() for m in re.finditer(pattern, str(seqrecord.seq))}
+        for start, end in matches.items():
+            seqrecord.features.append(
+                SeqFeature(FeatureLocation(start, end), type="CDS", id=name)
+            )
+
+    return seqrecord
+
+
+def create_and_annotate_record(sequence, seq_dataset=seq_dataset):
+    """Create a SeqRecord from an amino acid sequence string.
+
+    **Parameters**
+
+    **sequence**
+    > Sequence (`str`).
+    """
+    if seq_dataset is None:
+        seq_dataset = seq_dataset
+    protein = Seq(sequence)
+    protein_record = SeqRecord(
+        protein, id="example", annotations={"molecule_type": "protein"}
+    )
+    protein_record = annotate_record(protein_record)
+
+    return protein_record
+
+
+def convert_prosite_to_regex(prosite_string):
+    """Convert a PROSITE motif string to a regex string.
+
+    **Parameters**
+
+    **prosite_string**
+    > The PROSITE string (`str`).
+    """
+    # Implemented with a hack: by replacing characters, instead of using a lexer.
+    # See https://prosite.expasy.org/prosuser.html#conv_pa for definition.
+    # Remove period that ends the pattern:
+    if prosite_string[-1] == ".":
+        prosite_string = prosite_string[:-1]
+    else:
+        raise ValueError("Invalid format: a period ('.') must end the pattern")
+
+    # N- and C-terminal restrictions:
+    if "<" in prosite_string:
+        N_terminal = True
+        prosite_string = prosite_string.replace("<", "")
+    else:
+        N_terminal = False
+    if ">" in prosite_string:
+        if prosite_string[-1] != ">":
+            raise Exception("'>' inside square brackets is not supported yet")
+        C_terminal = True
+        prosite_string = prosite_string.replace(">", "")
+    else:
+        C_terminal = False
+
+    tokens = prosite_string.split("-")
+    regex_tokens = []
+    for token in tokens:
+        # Convert 'x' to regex: any amino acid, but don't match stop codons.
+        token = token.replace("x", "[^\\*]")
+
+        # Replace braces for exceptions.
+        if token[0] == "{":
+            token = token.replace("{", "[^")
+            token = token.replace("}", "]")
+
+        # Replace for repetition. Must come after exception replacement.
+        token = token.replace("(", "{")
+        token = token.replace(")", "}")
+
+        regex_tokens += [token]
+
+    regex = "".join(regex_tokens)
+    if N_terminal:
+        regex = "^" + regex
+    if C_terminal:
+        regex += "$"
+
+    return regex
